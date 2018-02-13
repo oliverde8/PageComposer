c1d96b3704d922e43342ae3e24a536dddf2fb088
root-dior-pim-lxc <root@dior-pim.lxc>
2018-02-13 10:27:05 +0100

    Added missing libraries to composer.json

---
+++ tests/Oliverde8/PageCompose/Tests/Service/BlockDefinitionsTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * File TestBlockDefinitions.php
+ *
+ * @author    de Cramer Oliver<oldec@smile.fr>
+ * @copyright 2018 Smile
+ */
+
+namespace Oliverde8\PageCompose\Tests\Service;
+
+use Oliverde8\PageCompose\Service\BlockDefinitions;
+
+class TestBlockDefinitions extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetPageBlocks()
+    {
+
+    }
+}
