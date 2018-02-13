8bb85b0c8482610367ef85e42f215106ad9c3bf4
Oliverde8 <oliverde8@gmail.com>
2018-02-13 15:11:29 +0100

    Fixed missing dependencies

---
+++ tests/Oliverde8/PageCompose/Tests/Service/UiComponentsTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File UiComponentsTest.php
+ *
+ * @author    de Cramer Oliver<oldec@smile.fr>
+ * @copyright 2018 Smile
+ */
+
+namespace Oliverde8\PageCompose\Tests\Service;
+
+use Oliverde8\PageCompose\Service\UiComponents;
+
+class UiComponentsTest extends \PHPUnit_Framework_TestCase
+{
+
+}
